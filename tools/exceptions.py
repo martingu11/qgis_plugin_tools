6a0bc23a79f0d4448d3bdab64db48a2e352730ca
Joonalai <joona@gispo.fi>
2020-09-08 12:17:43 +0300

    Improve exception handling

--- tools/exceptions.py
+++ tools/exceptions.py
@@ -3,19 +3,22 @@ __license__ = "GPL version 2"
 __email__ = "info@gispo.fi"
 __revision__ = "$Format:%H$"
 
-
 from typing import Dict, Optional
 
 
 class QgsPluginException(Exception):
     """ Use this as a base exception class in custom exceptions """
+    # Override default_msg to set default message in inherited classes
+    default_msg = ''
 
-    def __init__(self, message, bar_msg: Optional[Dict[str, str]] = None):
+    def __init__(self, message: Optional[str] = None, bar_msg: Optional[Dict[str, str]] = None):
         """
         Initializes the exception with custom bar_msg to be shown in message bar
         :param message: Title of the message
         :param bar_msg: dictionary formed by tools.custom_logging.bar_msg
         """
+        if message is None:
+            message = self.default_msg
         super().__init__(message)
         self.bar_msg = bar_msg
 
