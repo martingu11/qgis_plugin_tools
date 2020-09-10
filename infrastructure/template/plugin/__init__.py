05b1ebd33236adf427d6102e1801b349f05411c8
Joonalai <joona@gispo.fi>
2020-09-10 16:59:23 +0300

    Comment out imports in template

--- infrastructure/template/plugin/__init__.py
+++ infrastructure/template/plugin/__init__.py
@@ -1,10 +1,8 @@
-import os
+# <commented_out>from .qgis_plugin_tools.infrastructure.debugging import setup_pydevd
 
-from .qgis_plugin_tools.infrastructure.debugging import setup_pydevd
-
-if os.environ.get('QGIS_PLUGIN_USE_DEBUGGER') == 'pydevd':
-    if os.environ.get('IN_TESTS', "0") != "1" and os.environ.get('QGIS_PLUGIN_IN_CI', "0") != "1":
-        setup_pydevd()
+# <commented_out>if os.environ.get('QGIS_PLUGIN_USE_DEBUGGER') == 'pydevd':
+# <commented_out>    if os.environ.get('IN_TESTS', "0") != "1" and os.environ.get('QGIS_PLUGIN_IN_CI', "0") != "1":
+# <commented_out>        setup_pydevd()
 
 
 def classFactory(iface):
