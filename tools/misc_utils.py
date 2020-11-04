07bbb2e03036580a4177b072c8f2322db6190c96
Joonalai <joona@gispo.fi>
2020-11-04 11:56:26 +0200

    Add misc_tools and extent_to_bbox function

---
+++ tools/misc_utils.py
@@ -0,0 +1,24 @@
+__copyright__ = "Copyright 2020, Gispo Ltd"
+__license__ = "GPL version 3"
+__email__ = "info@gispo.fi"
+__revision__ = "$Format:%H$"
+
+from qgis.core import QgsRectangle
+
+
+def extent_to_bbox(extent: QgsRectangle, precision=2) -> str:
+    """
+       Add extent for the query
+
+       :param extent: QgsRectangle expected to be in the right extent
+       :param precision: Precision of coordinates
+       :return: string representation xmin,ymin,xmax,ymax
+       """
+    rnd = lambda c: round(c, precision)
+    bbox = (
+        rnd(extent.xMinimum()),
+        rnd(extent.yMinimum()),
+        rnd(extent.xMaximum()),
+        rnd(extent.yMaximum()),
+    )
+    return ','.join(map(str, bbox))
