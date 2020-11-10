5341c4c078d9b700680dd463fffb784cec92574f
Joonalai <joona@gispo.fi>
2020-11-10 13:18:51 +0200

    Add way to download to file with streaming

--- testing/test_network.py
+++ testing/test_network.py
@@ -6,7 +6,7 @@ __revision__ = "$Format:%H$"
 import pytest
 
 from ..tools.exceptions import QgsPluginNetworkException
-from ..tools.network import fetch
+from ..tools.network import fetch, download_to_file
 
 
 def test_fetch(new_project):
@@ -17,3 +17,25 @@ def test_fetch(new_project):
 def test_fetch_invalid_url(new_project):
     with pytest.raises(QgsPluginNetworkException):
         fetch('invalidurl')
+
+
+def test_download_to_file(new_project, tmpdir):
+    path_to_file = download_to_file('https://twitter.com/gispofinland/status/1324599933337567232/photo/1', tmpdir,
+                                    'test_file')
+    assert path_to_file.exists()
+
+
+def test_download_to_file_without_requests(new_project, tmpdir):
+    path_to_file = download_to_file('https://twitter.com/gispofinland/status/1324599933337567232/photo/1', tmpdir,
+                                    'test_file', use_requests_if_available=False)
+    assert path_to_file.exists()
+
+
+def test_download_to_file_invalid_url(new_project, tmpdir):
+    with pytest.raises(QgsPluginNetworkException):
+        download_to_file('invalidurl', tmpdir)
+
+
+def test_download_to_file_invalid_url_without_requests(new_project, tmpdir):
+    with pytest.raises(QgsPluginNetworkException):
+        download_to_file('invalidurl', tmpdir)
--- tools/network.py
+++ tools/network.py
@@ -1,5 +1,8 @@
 import logging
-from typing import Tuple
+from pathlib import Path
+from typing import Tuple, Optional
+import shutil
+import re
 
 from PyQt5.QtCore import QSettings, QUrl, QByteArray
 from PyQt5.QtNetwork import QNetworkRequest, QNetworkReply
@@ -10,6 +13,13 @@ from ..tools.exceptions import QgsPluginNetworkException
 from ..tools.i18n import tr
 from ..tools.resources import plugin_name
 
+try:
+    import requests
+    from requests.exceptions import RequestException
+except ImportError:
+    requests = None
+    RequestException = None
+
 __copyright__ = "Copyright 2020, Gispo Ltd"
 __license__ = "GPL version 3"
 __email__ = "info@gispo.fi"
@@ -17,7 +27,8 @@ __revision__ = "$Format:%H$"
 
 LOGGER = logging.getLogger(plugin_name())
 ENCODING = "utf-8"
-CONTENT_DISPOSITION = QByteArray(bytes("Content-Disposition", ENCODING))
+CONTENT_DISPOSITION_HEADER = "Content-Disposition"
+CONTENT_DISPOSITION_BYTE_HEADER = QByteArray(bytes(CONTENT_DISPOSITION_HEADER, ENCODING))
 
 
 def fetch(url: str, encoding: str = ENCODING) -> str:
@@ -25,17 +36,19 @@ def fetch(url: str, encoding: str = ENCODING) -> str:
     Fetch resource from the internet. Similar to requests.get(url) but is
     recommended way of handling requests in QGIS plugin
     :param url: address of the web resource
+    :param encoding: Encoding which will be used to decode the bytes
     :return: encoded string of the content
     """
     content, _ = fetch_raw(url, encoding)
     return content.decode(ENCODING)
 
 
-def fetch_raw(url, encoding: str = ENCODING) -> Tuple[bytes, str]:
+def fetch_raw(url: str, encoding: str = ENCODING) -> Tuple[bytes, str]:
     """
     Fetch resource from the internet. Similar to requests.get(url) but is
     recommended way of handling requests in QGIS plugin
     :param url: address of the web resource
+    :param encoding: Encoding which will be used to decode the bytes
     :return: bytes of the content and default name of the file or empty string
     """
     LOGGER.debug(url)
@@ -57,10 +70,63 @@ def fetch_raw(url, encoding: str = ENCODING) -> Tuple[bytes, str]:
 
     # https://stackoverflow.com/a/39103880/10068922
     default_name = ''
-    if reply.hasRawHeader(CONTENT_DISPOSITION):
-        header: QByteArray = reply.rawHeader(CONTENT_DISPOSITION)
+    if reply.hasRawHeader(CONTENT_DISPOSITION_BYTE_HEADER):
+        header: QByteArray = reply.rawHeader(CONTENT_DISPOSITION_BYTE_HEADER)
         default_name = bytes(header).decode(encoding).split('filename=')[1]
         if default_name[0] in ['"', "'"]:
             default_name = default_name[1:-1]
 
     return bytes(reply.content()), default_name
+
+
+def download_to_file(url: str, output_dir: Path, output_name: Optional[str] = None,
+                     use_requests_if_available: bool = True, encoding: str = ENCODING) -> Path:
+    """
+    Downloads a binary file to the file efficiently
+    :param url: Url of the file
+    :param output_dir: Path to the output directory
+    :param output_name: If given, use this as file name. Otherwise reads file name from
+    Content-Disposition header or uses the url
+    :param use_requests_if_available: Use Python package requests if it is available in the environment
+    :param encoding: Encoding which will be used to decode the bytes
+    :return: Path to the file
+    """
+
+    def get_output(default_filename) -> Path:
+        if output_name is None:
+            if default_filename != '':
+                out_name = default_filename
+            else:
+                out_name = url.replace('http://', '').replace('https://', '')
+                if len(output_name.split('/')[-1]) > 2:
+                    out_name = output_name.split('/')[-1]
+        else:
+            out_name = output_name
+        return Path(output_dir, out_name)
+
+    output = Path()
+
+    if use_requests_if_available and requests is not None:
+        # https://stackoverflow.com/a/39217788/10068922
+
+        try:
+            with requests.get(url, stream=True) as r:
+                try:
+                    r.raise_for_status()
+                except Exception:
+                    raise QgsPluginNetworkException(tr('Request failed with status code {}', r.status_code),
+                                                    bar_msg=bar_msg(r.text))
+                default_filename = re.findall("filename=(.+)", r.headers.get(CONTENT_DISPOSITION_HEADER, ''))
+                default_filename = default_filename[0] if len(default_filename) else ''
+                output = get_output(default_filename)
+                with open(output, 'wb') as f:
+                    shutil.copyfileobj(r.raw, f)
+        except RequestException as e:
+            raise QgsPluginNetworkException(tr('Request failed'), bar_msg=bar_msg(e))
+    else:
+        # Using simple fetch_raw
+        content, default_filename = fetch_raw(url, encoding)
+        output = get_output(default_filename)
+        with open(output, 'wb') as f:
+            f.write(content)
+    return output
